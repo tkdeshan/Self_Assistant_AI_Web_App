69597053354920f280bacd8f3b677f1ae0462e1f
tkdeshan <tkdeshan1103@gmail.com>
2024-01-07 10:19:22 +0530

    bug fix tailwindcss init

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,12 +2,7 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {
-      fontFamily: {
-        zenkaku: ["'Zen Kaku Gothic New'", "sans-serif"],
-      },
-      screens: {},
-    },
+    extend: {},
   },
   plugins: [],
 };
