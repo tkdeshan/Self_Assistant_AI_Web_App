544d00b729e91b72f14ce1113d0d35844315ebed
tkdeshan <tkdeshan1103@gmail.com>
2024-01-07 10:24:02 +0530

    add login to tailwindcss

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,7 +1,9 @@
 import React from "react";
 
 const LoginPage = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return <h1 className="text-3xl font-bold underline">
+      Login Page
+    </h1>;
 };
 
 export default LoginPage;
