b6b2f527956ee9cde0bcd52a7f13a30fc78b2f5b
tkdeshan <tkdeshan1103@gmail.com>
2024-01-07 17:22:37 +0530

    modify components

---
+++ frontend/src/components/TextBox.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { EyeIcon, EyeSlashIcon } from "@heroicons/react/24/outline";
+
+function TextBox({
+  label = null,
+  placeholder = "",
+  type = "text",
+  Icon = null,
+  value,
+  setValue,
+}) {
+  const isPassword = type === "password";
+
+  const [showPassword, setShowPassword] = React.useState(false);
+
+  const handleTogglePassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  return (
+    <div className="relative border-2 border-gray-300 w-full  rounded">
+      {label && (
+        <label className="absolute top-[-12px] left-2 bg-[#F9F9FA] px-1 text-gray-400 text-sm font-regular font-zenkaku">
+          {label}
+        </label>
+      )}
+      <div className="flex items-center p-2">
+        {Icon && <Icon className="h-6 w-6 text-gray-300 mr-2" />}
+        <input
+          type={isPassword && showPassword ? "text" : type}
+          placeholder={placeholder}
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          className="appearance-none bg-transparent border-none w-full h-full text-gray-700 p-2 leading-tight focus:outline-none"
+        />
+        {isPassword && (
+          <div className="ml-2 cursor-pointer" onClick={handleTogglePassword}>
+            {showPassword ? (
+              <EyeSlashIcon className="h-6 w-6 text-gray-300" />
+            ) : (
+              <EyeIcon className="h-6 w-6 text-gray-300" />
+            )}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default TextBox;
