d2fb523a2031e38a23ecfc61eadf35cd606653e1
tkdeshan <tkdeshan1103@gmail.com>
2024-01-07 10:16:34 +0530

    add routs and init login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,15 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+
 function App() {
-  return <div className="App">Start</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
