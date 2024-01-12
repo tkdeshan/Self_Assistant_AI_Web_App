b31a2376a4e4463c534d2a0d63ad11781cf20fd2
tkdeshan <tkdeshan1103@gmail.com>
2024-01-12 12:52:21 +0530

    add welcome page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
+import WelcomePage from "./pages/WelcomePage";
 import RegisterPage from "./pages/RegisterPage";
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<WelcomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
       </Routes>
