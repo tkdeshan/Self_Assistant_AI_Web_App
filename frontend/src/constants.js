5b8c57ff8c9063c77427e2869d43269ced977918
Kavinda Deshan <100216231+tkdeshan@users.noreply.github.com>
2024-04-27 22:19:05 +0530

    add exple to initial messege

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,6 +1,6 @@
 const messageGuide = {
   initial:
-    "Hello, please allow me to introduce myself. I am here to assist you in crafting your career path by analyzing your strengths and other key factors. I will ask you questions to guide this process. You can decide how many questions are needed. By increasing the number of questions, the accuracy will also increase. If you're ready, simply type 'yes' and the number of questions to proceed.",
+    "Hello, please allow me to introduce myself. I am here to assist you in crafting your career path by analyzing your strengths and other key factors. I will ask you questions to guide this process. You can decide how many questions are needed. By increasing the number of questions, the accuracy will also increase. If you're ready, simply type 'yes' and the number of questions to proceed. (Ex:- yes, 2)",
 };
 
 module.exports = {
