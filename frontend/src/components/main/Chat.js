b3b8572880ecff337c22ff97051405ab88d53b67
Kavinda Deshan <100216231+tkdeshan@users.noreply.github.com>
2024-04-27 13:21:23 +0530

    seperate text box and area field

--- frontend/src/components/main/Chat.js
+++ frontend/src/components/main/Chat.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import TextBox from "../TextBox";
+import TextArea from "../TextArea";
 import { PencilIcon } from "@heroicons/react/24/outline";
 import axios from "axios";
 import Button from "../Button";
@@ -108,7 +108,7 @@ function Chat() {
           e.preventDefault();
           await handleSend();
         }}>
-        <TextBox
+        <TextArea
           type="text"
           Icon={PencilIcon}
           value={chatInput}
