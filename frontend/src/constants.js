2c6cb632efa7e0cf2f72414f362409403e6e96f3
Kavinda Deshan <100216231+tkdeshan@users.noreply.github.com>
2024-05-05 13:42:05 +0530

    change constant message

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,11 +1,11 @@
 const messageGuide = {
   initial:
-    "Hello, please allow me to introduce myself. I am here to assist you in crafting your career path by analyzing your strengths and other key factors. I will ask you questions to guide this process. You can decide how many questions are needed. By increasing the number of questions, the accuracy will also increase. If you're ready, simply type 'yes' and the number of questions to proceed. (Ex:- yes, 2)",
+    "Hello, I am here to assist you in crafting your career path by analyzing your strengths and other key factors. I will ask you questions to guide this process. You can decide how many questions are needed. By increasing the number of questions, the accuracy will be also increase. If you're ready, simply type 'yes' and the number of questions to proceed. (Ex:- yes, 20)",
 };
 
 const messageTest = {
   initial:
-    "Please enter the skill you want to check. Also enter the number of question that you need. (Ex:- HTML, 2)",
+    "Hello, I am here to assist your knowledge. Please enter the skill you want to check and number of question that you need. By increasing the number of questions, the accuracy will be also increase. If you're ready, simply type skill and the number of questions to proceed.(Ex:- HTML, 20)",
 };
 
 module.exports = {
