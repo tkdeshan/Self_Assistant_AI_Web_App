55e439e592527adfa8478a5bec0cb4f62d2524b2
Kavinda Deshan <100216231+tkdeshan@users.noreply.github.com>
2024-05-05 20:50:03 +0530

    add skill charts

---
+++ frontend/src/components/Chart.js
@@ -0,0 +1,76 @@
+import React, { useState, useEffect } from "react";
+import Highcharts from "highcharts";
+import HighchartsReact from "highcharts-react-official";
+
+const Chart = ({ type, chartData }) => {
+  const [chartConfig, setChartConfig] = useState(null);
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    if (type === "column") {
+      const summaries = chartData.map((item) => item.summary);
+      const summaryData = summaries.map((summary, index) => {
+        const [skill, level] = summary.split(" : ");
+        let x = 0;
+        if (level === "Beginner") {
+          x = 1;
+        } else if (level === "Intermediate") {
+          x = 2;
+        } else if (level === "Advanced") {
+          x = 3;
+        } else if (level === "Expert") {
+          x = 4;
+        }
+
+        return { name: skill, y: x };
+      });
+      setData(summaryData);
+    }
+
+    const chartConfig = {
+      chart: {
+        type: type,
+      },
+      xAxis: {
+        xAxis: {
+          type: "category",
+        },
+        title: {
+          text: null,
+        },
+
+        min: 0,
+        max: 5,
+        scrollbar: {
+          enabled: true,
+        },
+        tickLength: 0,
+      },
+      yAxis: {
+        title: {
+          text: null,
+        },
+      },
+
+      series: [
+        {
+          name: "Skills",
+          colorByPoint: true,
+          data: data,
+        },
+      ],
+    };
+
+    setChartConfig(chartConfig);
+
+    // eslint-disable-next-line
+  }, []);
+
+  return (
+    <div>
+      <HighchartsReact highcharts={Highcharts} options={chartConfig} />
+    </div>
+  );
+};
+
+export default Chart;
--- frontend/src/components/main/Dashboard.js
+++ frontend/src/components/main/Dashboard.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import Chart from "../Chart";
 
 function Dashboard() {
   const [careerAnnalys, setCareerAnnalys] = useState(null);
@@ -25,32 +26,46 @@ function Dashboard() {
 
   return (
     <div className="flex flex-row gap-4 px-8 py-4 h-screen overflow-auto" style={{ maxHeight: "100%" }}>
-      <div className="flex flex-col w-1/2 p-4 bg-green-200 rounded-md shadow-md">
-        <h1 className="text-gray-500 font-bold tracking-wider underline text-xl">Career Guidance Analysis</h1>
+      <div className="flex flex-col w-1/2 p-4 bg-green-200 rounded-md shadow-md overflow-auto">
+        <h1 className="text-gray-500 font-bold tracking-wider underline text-xl text-center">
+          Career Guidance Analysis
+        </h1>
 
         {careerAnnalys?.length > 0 ? (
-          <div className="flex flex-col ml-4">
+          <div className="flex flex-col ml-4 mt-2">
             {careerAnnalys?.map((item, idx) => (
-              <div className="flex flex-row gap-5 mt-5" key={idx}>
-                <div className="">
-                  {item.summary.split("\n").map((line, index) => {
-                    if (line.startsWith("**")) {
-                      return (
-                        <div key={index}>
-                          <strong>{line.trim().replace(/\*+/g, "")}</strong>
-                          <br />
-                        </div>
-                      );
-                    } else {
-                      return (
-                        <div className="" key={index}>
-                          {line}
-                        </div>
-                      );
-                    }
-                  })}
+              <div>
+                <div className="flex flex-row gap-10 mt-2 mb-5" key={idx}>
+                  <div>
+                    <div>
+                      {new Date(item?.date).toLocaleDateString(undefined, { day: "numeric", month: "long" })},{" "}
+                      {new Date(item?.date).toLocaleTimeString(undefined, {
+                        hour: "numeric",
+                        minute: "numeric",
+                        hour12: true,
+                      })}
+                    </div>
+                  </div>
+                  <div className="">
+                    {item.summary.split("\n").map((line, index) => {
+                      if (line.startsWith("**")) {
+                        return (
+                          <div key={index}>
+                            <strong>{line.trim().replace(/\*+/g, "")}</strong>
+                            <br />
+                          </div>
+                        );
+                      } else {
+                        return (
+                          <div className="" key={index}>
+                            {line}
+                          </div>
+                        );
+                      }
+                    })}
+                  </div>
                 </div>
-                <div>{new Date(item?.date).toLocaleString()}</div>
+                <Chart type="pie" chartData={careerAnnalys} />
               </div>
             ))}
           </div>
@@ -59,17 +74,31 @@ function Dashboard() {
         )}
       </div>
 
-      <div className="flex flex-col w-1/2 p-4 bg-green-200 rounded-md shadow-md">
-        <h1 className="text-gray-500 font-bold tracking-wider underline text-xl">Knowledge Test Analysis</h1>
+      <div className="flex flex-col w-1/2 p-4 bg-green-200 rounded-md shadow-md overflow-auto">
+        <h1 className="text-gray-500 font-bold tracking-wider underline text-xl text-center">
+          Knowledge Test Analysis
+        </h1>
 
         {testAnnalys?.length > 0 ? (
-          <div className="flex flex-col ml-4">
+          <div className="flex flex-col ml-4 mt-2">
             {testAnnalys?.map((item, idx) => (
-              <div className="flex flex-row gap-5 mt-5" key={idx}>
+              <div className="flex flex-row gap-10" key={idx}>
+                <div>
+                  <div>
+                    {new Date(item?.date).toLocaleDateString(undefined, { day: "numeric", month: "long" })},{" "}
+                    {new Date(item?.date).toLocaleTimeString(undefined, {
+                      hour: "numeric",
+                      minute: "numeric",
+                      hour12: true,
+                    })}
+                  </div>
+                </div>
                 <div>{item?.summary}</div>
-                <div>{new Date(item?.date).toLocaleString()}</div>
               </div>
             ))}
+            <div className="mt-4">
+              <Chart type="column" chartData={testAnnalys} />
+            </div>
           </div>
         ) : (
           <div className="mt-1">Please first go to chat and follow the steps...</div>
